1f098deb815cd699e6e83d77e602ff45e112c040
praszxc <bpras747@gmail.com>
2023-03-29 06:15:22 +0700

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    public function index() {
+        $posts = Post::all();
+        //return response()->json([ 'data' => $posts]);
+        return PostResource::collection($posts);
+    }
+}
